1131c53e0023e5faee50a35f6f5f0a448ab77c7a
Lucas Monteiro <lmontecosta@gmail.com>
2025-04-04 17:32:49 -0300

    perf: cria alertas com validações

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 let qtdPista = 100;
 let qtdSuperior = 200;
 let qtdInferior = 400;
-document.getElementById('qtd-pista').innerHTML = qtdPista
-document.getElementById('qtd-superior').innerHTML = qtdSuperior
-document.getElementById('qtd-inferior').innerHTML = qtdInferior
+document.getElementById('qtd-pista').innerHTML = qtdPista;
+document.getElementById('qtd-superior').innerHTML = qtdSuperior;
+document.getElementById('qtd-inferior').innerHTML = qtdInferior;
 
 //comprar ticket*
 function comprar() {
@@ -12,19 +12,32 @@ function comprar() {
     //comprar a quantidade de ingresso informada
     let quantidade = document.getElementById('qtd').value;
 
+    if (isNaN(quantidade) || quantidade <= 0) {
+        alert(`Insira uma quantidade válida`);
+        return;
+    }
+
     reduzirEstoque(tipoIngresso, quantidade);
+    limparFormulario();
 }
 
 //deduzir ticket da quantidade disponível
 function reduzirEstoque(tipo, qtd) {
-    if (tipo == 'pista') {
+    if (tipo == 'pista' && qtd <= qtdPista) {
         qtdPista -= qtd;
-        document.getElementById('qtd-pista').innerHTML = qtdPista
-    } else if (tipo == 'superior') {
+        document.getElementById('qtd-pista').innerHTML = qtdPista;
+    } else if (tipo == 'superior' && qtd <= qtdSuperior) {
         qtdSuperior -= qtd;
-        document.getElementById('qtd-superior').innerHTML = qtdSuperior
-    } else {
+        document.getElementById('qtd-superior').innerHTML = qtdSuperior;
+    } else if (tipo == 'inferior' && qtd <= qtdInferior) {
         qtdInferior -= qtd;
-        document.getElementById('qtd-inferior').innerHTML = qtdInferior
+        document.getElementById('qtd-inferior').innerHTML = qtdInferior;
+    } else {
+        alert(`Esse ingresso não está disponível no momento. Tente mais tarde`);
+        return;
     }
+}
+
+function limparFormulario() {
+    document.getElementById('qtd').value = ''
 }

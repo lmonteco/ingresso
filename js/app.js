62aa08e5364300b6261c52d1a645587c0cfce432
Lucas Monteiro <lmontecosta@gmail.com>
2025-04-04 13:28:43 -0300

    feat: estabelece a essência da função de compra

---
+++ js/app.js
@@ -0,0 +1,8 @@
+//comprar ticket*
+function comprar() {
+    //comprar o tipo de ingresso escolhido*
+    let tipoIngresso = document.getElementById('tipo-ingresso').value;
+    //comprar a quantidade de ingresso informada
+    let quantidade = document.getElementById('qtd').value;
+}
+//deduzir ticket da quantidade disponível

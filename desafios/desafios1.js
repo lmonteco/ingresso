c398078a0d54ec37789f89e8d115a3b2b4b89c2d
Lucas Monteiro <lmontecosta@gmail.com>
2025-04-04 18:22:11 -0300

    challenge: adiciona desafios ao git

---
+++ desafios/desafios1.js
@@ -0,0 +1,8 @@
+function converter(string) {
+    console.log(typeof string);    
+    let conversaoInt = parseInt(string);
+    console.log(conversaoInt);
+    console.log(typeof conversaoInt);
+}
+
+converter('100');
